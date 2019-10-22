4f3e289b57d2f2e5536dc3f7d05e5cef36a30943
Mark McLaren <mark.mclaren@mclarencollege.in>
2019-10-22 01:32:23 +0100

    Initial commit - just a simple icon in center of screen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Center(
+        child: Icon(
+          Icons.location_on,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+    );
+  }
+}

543697823e83c2f7a5b48c964d74b1ca06f7a389
rahul masih <rahulmasih61@gmail.com>
2019-10-23 14:28:23 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animation Demo',
       home: Center(
-        child: AnimatedButton(),
+        child: HomePage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(child: AnimatedButton(icon: Icon(Icons.thumb_up,color: Colors.red,size: 60,),)),
+          Center(child: AnimatedButton(icon: Icon(Icons.favorite,color: Colors.red,size: 60,),)),
+          Center(child: AnimatedButton(icon: Icon(Icons.notifications_active,color: Colors.red,size: 60,),)),
+        ],
       ),
     );
   }
 }
 
 class AnimatedButton extends StatefulWidget {
+  Icon icon;
+
+  AnimatedButton({this.icon});
+
   @override
   _AnimatedButtonState createState() => _AnimatedButtonState();
 }
 
-class _AnimatedButtonState extends State<AnimatedButton> {
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
 
+  @override
+  void initState() {
+    controller = AnimationController(
+        duration: Duration(milliseconds: 1200), vsync: this, value: 0);
+    controller.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+    controller.addListener(() {
+      print(controller.value);
+      setState(() {});
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    CurvedAnimation smoothAnimation =
+        CurvedAnimation(parent: controller, curve: Curves.easeInExpo);
     return GestureDetector(
-      onTap: toggleVisible,
-      child: Icon(
-        Icons.location_on,
+      onTap: () {
+        controller.forward();
+      },
+      child: Container(
+        width: 150,
+        height: 150,
         color: Colors.white,
-        size: 60,
+        child: Transform.scale(
+          scale: Tween(begin: 1.0, end: 1.5).transform(controller.value),
+          child: widget.icon,
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   void toggleVisible() {
